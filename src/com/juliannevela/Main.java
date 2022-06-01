6011466e21728ec14ff20fbbfcc0b9df4d441d85
Julianne Vela <hello@juliannevela.dev>
2022-06-01 13:32:27 -0700

    added payment schedule logic and refactored main method to clean up code

--- src/com/juliannevela/Main.java
+++ src/com/juliannevela/Main.java
@@ -4,20 +4,38 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PERCENT = 100;
+
+    public static String convertCurrency(double balance) {
+        return NumberFormat.getCurrencyInstance().format(balance);
+    }
     public static void main(String[] args) {
         int principal = (int)readPrompt("Principal ($1K - $1M)", 1000, 1_000_000);
         float annualInterest = (float)readPrompt("APR", 1, 30);
         byte years = (byte)readPrompt("Term length", 10, 30);
 
+        printMortgage(principal, annualInterest, years);
+
+        printPaymentSchedule(principal, annualInterest, years);
+
+    }
+
+    private static void printMortgage(int principal, float annualInterest, byte years) {
         double mortgage = calculateMortgage(principal, annualInterest, years);
         System.out.println("Mortgage: ");
         System.out.println("=====================");
-        System.out.println("Monthly Payments: " + NumberFormat.getCurrencyInstance().format(mortgage));
-        System.out.println("Payment Schedule: ");
-        System.out.println("=====================");
-        // display list of remaining balance after each payment. 
+        System.out.println("Monthly Payments: " + convertCurrency(mortgage));
     }
 
+    private static void printPaymentSchedule(int principal, float annualInterest, byte years) {
+        System.out.println();
+        System.out.println("Payment Schedule: ");
+        for(short month = 1; month < years * MONTHS_IN_YEAR; month++) {
+            double balance = calculateBalance(principal, annualInterest, years, month);
+            System.out.println(month + ": " + convertCurrency(balance));
+        }
+    }
     public static double readPrompt(String question, double min, double max) {
         Scanner scanner = new Scanner(System.in);
         double value;
@@ -31,15 +49,21 @@ public class Main {
         }
         return value;
     }
-
     public static double calculateMortgage(int principal, float annualInterest, byte years) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
-
         float monthlyRate = annualInterest / PERCENT / MONTHS_IN_YEAR;
         short totalPayments = (short)(years * MONTHS_IN_YEAR);
+
         return principal
                 * (monthlyRate * Math.pow(1 + monthlyRate, totalPayments)
                 / (Math.pow(1 + monthlyRate, totalPayments) - 1));
+    }
+
+    public static double calculateBalance(int principal, float annualInterest, byte years, short completedNumberOfPayments) {
+        float monthlyRate = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        short totalPayments = (short)(years * MONTHS_IN_YEAR);
+
+        return principal
+                    * (Math.pow(1+monthlyRate, totalPayments) - Math.pow(1+monthlyRate, completedNumberOfPayments))
+                    / (Math.pow(1+monthlyRate, totalPayments) - 1);
     }
 }
